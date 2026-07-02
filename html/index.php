7cf7bd38a03726321177e00aaafe754bc6840220
Philippe Poilbarbe <ppoilbarbe@users.noreply.github.com>
2026-07-02 18:30:03 +0200

    feat : fenêtre surgissante avec la traduction du texte Tengwar de la bannière
    - html/index.php : un clic sur l'image Sindarin.jpg affiche une modale
      contenant la traduction française du texte en anglais phonémique
      écrit en Tengwars. Aucune indication visuelle (pas de lien, pas de
      changement de curseur) ne signale que l'image est cliquable.
    - html/css/general.css : styles de la modale (overlay, boîte), alignés
      sur la palette du site.

--- html/index.php
+++ html/index.php
@@ -21,9 +21,25 @@ do_page_open('', -1, ['logo' => true]);
 writeLine('<h1 class="HomeTitle">Philippe Poilbarbe</h1>');
 writeLine('<p class="HomeSubtitle">Cardolan</p>');
 writeLine('<hr class="HomeRule">');
-writeLine('<p align="center">' . imageLink('', 'Sindarin.jpg', '', 'Sindarin-img') . '<br>');
+writeLine('<p align="center">' . imageLink('', 'Sindarin.jpg', '', 'Sindarin-img', 'Sindarin-img') . '<br>');
 writeLine('<br>Ce site a été testé avec Chromium et FireFox.<br>');
 writeLine('Optimisé pour une résolution minimale de 1024x768.<br><br><br><br>');
+
+writeLine('<div id="Sindarin-modal" class="Sindarin-modal" hidden>');
+writeLine('  <div class="Sindarin-modal-box">');
+writeLine('    <p>Traduction de l\'anglais phonémique écrit en Tengwars&nbsp;: «&nbsp;Welcome to the home site of Philippe Poilbarbe, as known as Marcel Spock&nbsp;», soit, à peu près «&nbsp;Bienvenue sur le site personnel de Philippe Poilbarbe, aussi connu sous le nom de Marcel Spock&nbsp;».</p>');
+writeLine('  </div>');
+writeLine('</div>');
+writeLine('<script>');
+writeLine('(function () {');
+writeLine('  var img = document.images["Sindarin-img"];');
+writeLine('  var modal = document.getElementById("Sindarin-modal");');
+writeLine('  if (!img || !modal) return;');
+writeLine('  img.addEventListener("click", function () { modal.hidden = false; });');
+writeLine('  modal.addEventListener("click", function () { modal.hidden = true; });');
+writeLine('  document.addEventListener("keydown", function (e) { if (e.key === "Escape") modal.hidden = true; });');
+writeLine('})();');
+writeLine('</script>');
 writeLine(imageLink(
     'https://www.april.org/adherer?referent=Philippe+POILBARBE+%28ppoilbarbe%29',
     'http://www.april.org/files/association/documents/bannieres/banniere_horizontale_soutien_adherent_fulltext_486_par_60.png',
